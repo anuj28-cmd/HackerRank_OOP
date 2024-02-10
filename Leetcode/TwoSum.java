ea466b96b5a61bd51dfb52528977b1e34a0528ff
anuj28-cmd <153478127+anuj28-cmd@users.noreply.github.com>
2024-02-10 16:07:39 +0530

    yes

---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,25 @@
+package HackerRank_OOP.Leetcode;
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i =0; i<nums.length-1;i++){
+            for(int j =i+1; j<nums.length ; j++){
+                if(nums[i]+nums[j]==target){
+                    // System.out.println(i+j+" = "+target);
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
+
+class TwoSum{
+    public static void main(String[] args){
+        int[] nums={2,7,11,15};
+        Solution S1 =new Solution();
+        int[] result = S1.twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+        }
+    }
+
